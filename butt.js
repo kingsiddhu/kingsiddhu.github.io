3fc40ff12765973af441691ac01d764260da3dc0
siddharth <72471456+kingsiddhu@users.noreply.github.com>
2021-06-09 06:23:04 -0300

    Create butt.js

---
+++ butt.js
@@ -0,0 +1,23 @@
+document.write('\
+\  
+             <div>\
+\
+               <h1>CONTACT US</h1>\
+\
+               <a href="mailto:help.siddhu@gmail.com" target="_blank"><img src="gmail.png" style="max-width: 50px"></a>\
+\
+               <a href="https://www.instagram.com/king_siddharth__/" target="_blank"><img src="insta.png" style="max-width: 50px"></a>\
+\
+               <a href="https://www.paypal.me/donatemeking" target="_blank"><img src="do.jpg" style="max-width: 75px"></a>\
+\
+               <a href="secret.html" style="max-width: 40%" target="_blank"><img src="s.png"></a>\
+\
+               <p>For any removal email to me by clicking the gmail icon. And please don't report this website.</p>\
+\
+               <br>\
+\
+               <a href="https://kingsiddhu.github.io/" target="_parent" style="color: black; font-size: 150%">BACK TO THE MAIN PAGE</a>\
+\
+             </div>\
+\
+');
